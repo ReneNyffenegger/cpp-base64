b1d4cd625791c9193762b3868dd0ab904499aede
Q <min9fore@gmail.com>
2017-05-22 20:18:32 +0900

    Update base64.h
    better to add header guard

--- base64.h
+++ base64.h
@@ -1,4 +1,9 @@
+#ifndef __BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A__
+#define __BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A__
+
 #include <string>
 
 std::string base64_encode(unsigned char const* , unsigned int len);
 std::string base64_decode(std::string const& s);
+
+#endif /* __BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A__ */
